04e5f10208ebcd15dfc26c48316254b71c58b3ff
MZhang-9 <m2volleyballa@gmail.com>
2024-10-15 18:05:52 -0400

    redid button, i dont know where my old code went I had it saved down so I had to recopy and paste, had to readd face a b c back to the images not sure what happened going to try and fix everything else now

---
+++ InstructionScreen.java
@@ -0,0 +1,44 @@
+import greenfoot.*;
+
+public class InstructionScreen extends World {
+    private MenuScreen menu;
+    private String[] instructions;
+    private int index;
+    private Label instructionLabel;
+
+    public InstructionScreen(MenuScreen menu) {
+        super(600, 400, 1);
+        this.menu = menu;
+        instructions = new String[]{
+            "Welcome to the game!",
+            "Use arrow keys to move.",
+            "Press space to jump."
+        };
+        index = 0;
+
+        instructionLabel = new Label(instructions[index], 24);
+        addObject(instructionLabel, 300, 200);
+        
+        addObject(new Button(this::nextInstruction, "Next"), 450, 350);
+        addObject(new Button(this::previousInstruction, "Previous"), 150, 350);
+        addObject(new Button(this::goBack, "Back to Menu"), 300, 350);
+    }
+
+    public void nextInstruction() {
+        if (index < instructions.length - 1) {
+            index++;
+            instructionLabel.setText(instructions[index]);
+        }
+    }
+
+    public void previousInstruction() {
+        if (index > 0) {
+            index--;
+            instructionLabel.setText(instructions[index]);
+        }
+    }
+
+    public void goBack() {
+        Greenfoot.setWorld(menu);
+    }
+}
--- MenuScreen.java
+++ MenuScreen.java
@@ -1,8 +1,30 @@
 import greenfoot.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class MenuScreen extends World {
     private Queue<GreenfootImage> avatars;
     private GreenfootImage currentAvatar;
+
+    public MenuScreen() {    
+        super(600, 400, 1);
+        addObject(new Button(this::goInstructions, "Instructions"), 300, 340);
+
+        // Avatar Selection
+        avatars = new LinkedList<>();
+        avatars.add(new GreenfootImage("face_a.png"));
+        avatars.add(new GreenfootImage("face_b.png"));
+        avatars.add(new GreenfootImage("face_c.png"));
+
+        currentAvatar = avatars.peek();
+        setBackground(currentAvatar);
+
+        addObject(new Button(this::cycleAvatar, "Next Avatar"), 300, 300);
+    }
+
+    public void goInstructions() {
+        Greenfoot.setWorld(new InstructionScreen(this));
+    }
 
     public void cycleAvatar() {
         avatars.add(avatars.remove()); // Rotate the avatars in the queue

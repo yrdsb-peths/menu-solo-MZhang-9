8d553113d11b3675e654bef604d51ee4fb9a6a45
MZhang-9 <m2volleyballa@gmail.com>
2024-10-19 10:58:44 -0400

    Update MenuScreen.java

--- MenuScreen.java
+++ MenuScreen.java
@@ -5,11 +5,13 @@ import java.util.LinkedList;
 public class MenuScreen extends World {
     private Queue<GreenfootImage> avatars;
     private GreenfootImage currentAvatar;
+    private Actor avatarActor;
 
     public MenuScreen() {    
         super(600, 400, 1);
         addObject(new Button(this::goInstructions, "Instructions"), 300, 360);
         addObject(new Button(this::goHighScores, "High Scores"), 300, 260); 
+        addObject(new Button(this::cycleAvatar, "Next Avatar"), 300, 310);
 
         // Avatar Selection
         avatars = new LinkedList<>();
@@ -18,9 +20,13 @@ public class MenuScreen extends World {
         avatars.add(new GreenfootImage("face_c.png"));
 
         currentAvatar = avatars.peek();
-        setBackground(currentAvatar);
-
-        addObject(new Button(this::cycleAvatar, "Next Avatar"), 300, 310);
+        avatarActor = new Actor() { // Create an anonymous Actor class for the avatar
+            @Override
+            public GreenfootImage getImage() {
+                return currentAvatar;
+            }
+        };
+        updateAvatar();
     }
     
     public void goInstructions() {
